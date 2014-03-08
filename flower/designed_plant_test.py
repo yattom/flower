9d063d23eea96c2b8322f9e755f11e82c784c5f6
yattom <tsutomu.yasui@gmail.com>
2014-03-08 11:26:19 +0900

    Proxy with recorded operations

--- flower/designed_plant_test.py
+++ flower/designed_plant_test.py
@@ -110,44 +110,69 @@ def has_no_part(name):
 
 import operator
 class Proxy(object):
+    class Storage(object):
+        pass
+
+    class Attr(object):
+        def __init__(self, name):
+            self.name = name
+        def evaluate(self, subject):
+            return subject.__getattribute__(self.name)
+
+    class GetItem(object):
+        def __init__(self, idx):
+            self.idx = idx
+        def evaluate(self, subject):
+            return operator.getitem(subject, self.idx)
+
+    class Call(object):
+        def __init__(self, args, kwargs):
+            self.args = args
+            self.kwargs = kwargs
+        def evaluate(self, subject):
+            return subject.__call__(*self.args, **self.kwargs)
+
     def __init__(self, subject):
-        self._v.subject = subject
-        self._v.operations = []
+        self.__v = Proxy.Storage()
+        self.__v.subject = subject
+        self.__v.operations = []
 
     def __getattr__(self, name):
         print "attr: " + name
-        subj = self._v.operations[-1][1] if self._v.operations else self._v.subject
-        op = (subj.__getattribute__, (name,), {})
-        val = subj.__getattribute__(name)
-        self._v.operations.append((op, val))
+        subj = self.__v.operations[-1][1] if self.__v.operations else self.__v.subject
+        op = Proxy.Attr(name)
+        val = op.evaluate(subj)
+        self.__v.operations.append((op, val))
         return self
 
     def __getitem__(self, idx):
-        subj = self._v.operations[-1][1] if self._v.operations else self._v.subject
-        op = (operator.getitem, (subj, idx), {})
-        val = operator.getitem(subj, idx)
-        self._v.operations.append((op, val))
+        subj = self.__v.operations[-1][1] if self.__v.operations else self.__v.subject
+        op = Proxy.GetItem(idx)
+        val = op.evaluate(subj)
+        self.__v.operations.append((op, val))
         return self
 
     def __call__(self, *args, **kwargs):
-        subj = self._v.operations[-1][1] if self._v.operations else self._v.subject
-        op = (subj.__call__, args, kwargs)
-        val = subj.__call__(*args, **kwargs)
-        self._v.operations.append((op, val))
+        subj = self.__v.operations[-1][1] if self.__v.operations else self.__v.subject
+        op = Proxy.Call(args, kwargs)
+        val = op.evaluate(subj)
+        self.__v.operations.append((op, val))
         return self
 
     def current(self):
-        subj = self._v.operations[-1][1] if self._v.operations else self._v.subject
+        subj = self.__v.subject
+        for op, _ in self.__v.operations:
+            subj = op.evaluate(subj)
         return subj
 
     def before(self):
-        subj = self._v.operations[-1][1] if self._v.operations else self._v.subject
+        subj = self.__v.operations[-1][1] if self.__v.operations else self.__v.subject
         return subj
 
 
 class Fixture(Proxy):
     def __init__(self, subject):
-        self.subject = subject
+        super(Fixture, self).__init__(subject)
 
     def __enter__(self):
         return self
